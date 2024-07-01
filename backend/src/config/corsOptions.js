702ed97c29604208d08662995973ca04e6b7dceb
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-01 03:01:42 +0300

    cors options added

---
+++ backend/src/config/corsOptions.js
@@ -0,0 +1,16 @@
+const allowedOrigins = require("./allowedOrigins");
+const corsOptions = {
+  origin: (origin, callback) => {
+    const isDevelopment = process.env.ENV === "development" ? true : false;
+    const allowEmptyOrigin = isDevelopment && !origin ? true : false;
+    if (allowedOrigins.indexOf(origin) !== -1 || allowEmptyOrigin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
