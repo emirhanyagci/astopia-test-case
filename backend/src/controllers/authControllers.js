972cc857920b5b591cb6296f914f08cd86d46602
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-01 16:21:12 +0300

    Auth controllers completed

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,4 +1,108 @@
 const asyncHandler = require("express-async-handler");
-exports.login = asyncHandler((req, res, next) => {});
-exports.signup = asyncHandler((req, res, next) => {});
-exports.refresh = asyncHandler((req, res, next) => {});
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+// @desc login
+// @route POST /auth/login
+// @access Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).exec();
+  if (!user) {
+    res.status(401).json({
+      message: "User not found with received data",
+    });
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    res.status(401).json({
+      message: "User not found with received data",
+    });
+  }
+  const accessToken = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+  const refreshToken = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+  res.cookie("jwt", refreshToken, {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+  res.json({ accessToken });
+});
+// @desc signup
+// @route POST /auth/signup
+// @access Public
+exports.signup = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  //check is email exist
+  const duplicateUser = await User.findOne({ email }).exec();
+  if (duplicateUser) {
+    res.status(409).json({
+      message: "User already exist with this email",
+    });
+  }
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const user = await User.create({ email, password: hashedPassword });
+  if (user) {
+    res.json({
+      message: "User succesfully created",
+    });
+  } else {
+    res.status(400).json({
+      message: "Invalid user data received",
+    });
+  }
+});
+// @desc refresh for re auth
+// @route GET /auth/refresh
+// @access public
+exports.refresh = asyncHandler((req, res, next) => {
+  const cookies = req.cookies;
+  if (!cookies) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+  const refreshToken = cookies.jwt;
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    asyncHandler(async (err, decoded) => {
+      if (err) return res.status(403).json({ message: "Forbidden" });
+      const user = await User.findOne({
+        username: decoded.username,
+      }).exec();
+      if (!user) return res.status(401).json({ message: "Unauthorized" });
+      const accessToken = jwt.sign(
+        {
+          id: user._id,
+          email: user.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "15m" }
+      );
+      res.json({ accessToken });
+    })
+  );
+});
+// @desc clear cookie
+// @route GET /auth/logout
+// @access public
+exports.logout = asyncHandler((req, res, next) => {
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+  res.json({ message: "Cookie cleared" });
+});
