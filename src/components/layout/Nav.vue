617c28090a117b5ebe72054e46fca3073cd1ec60
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-02 17:27:44 +0300

    nav created

---
+++ src/components/layout/Nav.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+<template>
+  <nav class="bg-white shadow-nav flex justify-center p-2">
+    <div class="container flex items-center justify-between">
+      <div>
+        <img
+          src="/public/logo.png"
+          alt=""
+          class="w-40 sm:w-48 md:w-52 xl:w-64"
+        />
+      </div>
+      <div>
+        <button class="shadow-button py-1 px-2 rounded-lg">Login</button>
+        <button class="shadow-button py-1 px-2 rounded-lg">Logout</button>
+      </div>
+    </div>
+  </nav>
+</template>
+<style></style>
