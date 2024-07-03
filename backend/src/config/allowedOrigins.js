62189205cb95f2e4672e7906dbccdd79810930ad
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-03 16:50:00 +0300

    server:vercel origin added to allowedOrigin config

--- backend/src/config/allowedOrigins.js
+++ backend/src/config/allowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://astopia-test-case-frontend.vercel.app",
+];
 
 module.exports = allowedOrigins;
