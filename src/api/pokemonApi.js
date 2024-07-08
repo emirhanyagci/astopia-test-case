4edc4a87ae7acaedb204e4b3b265106fcc533c9d
Emirhan Yagci <emirhan.yacis@gmail.com>
2024-07-08 13:42:23 +0300

    client:pokemonApi file updated for cors bug

--- src/api/pokemonApi.js
+++ src/api/pokemonApi.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 import { POKE_PER_PAGE, MAX_POKE_COUNT } from "../helpers/constants";
 const axiosInstance = axios.create({
-  baseURL: `https://pokeapi.co/api/v2`,
+  baseURL: `https://pokeapi.co/api/v2/pokemon`,
 });
 export const getPokemons = (page) => {
   const offSet = POKE_PER_PAGE * (page - 1);
-
-  return axiosInstance.get(`/pokemon?offset=${offSet}&limit=${POKE_PER_PAGE}`);
+  return axiosInstance.get(`?offset=${offSet}&limit=${POKE_PER_PAGE}`);
 };
 
 export const getPokemonWithId = (pokeId) => {
-  return axiosInstance.get(`/pokemon/${pokeId}`);
+  return axiosInstance.get(`/${pokeId}`);
 };
 export const getPokemonWithUrl = (url) => {
   return axios.get(url);
